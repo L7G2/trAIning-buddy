d902497a1ce0de8e994418186acfe576a27fa622
Sebastian Zieliński <zielis277@wp.pl>
2025-05-04 04:32:48 +0200

    Frontend: Navbar added

---
+++ frontend/trAIning-buddy/src/components/userNotLoggedIn/navigation/NotLoggedNav.tsx
@@ -0,0 +1,51 @@
+import { NavLink, Outlet } from "react-router-dom";
+import classes from "./NotLoggedNav.module.css";
+
+function NotLoggedNav() {
+  return (
+    <>
+      <nav className={classes.navigation}>
+        <NavLink to="/" className={classes.image}>
+          trAIningbuddy
+        </NavLink>
+        <div className={classes.links}>
+          <ul className={classes.ul}>
+            <li className={classes.li}>
+              <NavLink
+                to="about"
+                className={({ isActive }) =>
+                  isActive ? classes.active : classes.notactive
+                }
+              >
+                O nas
+              </NavLink>
+            </li>
+            <li className={classes.li}>
+              <NavLink
+                to="coaches"
+                className={({ isActive }) =>
+                  isActive ? classes.active : classes.notactive
+                }
+              >
+                Nasi trenerzy
+              </NavLink>
+            </li>
+            <li className={classes.li}>
+              <NavLink
+                to="login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : classes.notactive
+                }
+              >
+                Zaloguj się
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
+
+export default NotLoggedNav;
