2b67fa15cfe3da351a769a3340ac0304e277b3df
Sebastian Zieliński <zielis277@wp.pl>
2025-05-04 04:32:48 +0200

    Frontend: Navbar added

--- frontend/trAIning-buddy/src/App.tsx
+++ frontend/trAIning-buddy/src/App.tsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import NotLoggedNav from "./components/userNotLoggedIn/navigation/NotLoggedNav";
+import AboutPage from "./components/userNotLoggedIn/aboutPage/AboutPage";
+import CoachesPage from "./components/userNotLoggedIn/coachesPage/CoachesPage";
+import LoginPage from "./components/userNotLoggedIn/loginPage/LoginPage";
+import MainPage from "./components/userNotLoggedIn/mainPage/MainPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NotLoggedNav />,
+    id: "main",
+    children: [
+      { index: true, element: <MainPage /> },
+      { path: "about", element: <AboutPage /> },
+      { path: "coaches", element: <CoachesPage /> },
+      { path: "login", element: <LoginPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <h1>Witaj siłaczu :D</h1>;
+  return <RouterProvider router={router} />;
 }
 export default App;
---
+++ frontend/trAIning-buddy/src/components/userNotLoggedIn/coachesPage/CoachesPage.tsx
@@ -0,0 +1,5 @@
+function CoachesPage() {
+  return <h1>CoachesPage</h1>;
+}
+
+export default CoachesPage;
