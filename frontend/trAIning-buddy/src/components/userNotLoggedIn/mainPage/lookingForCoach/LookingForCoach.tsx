e9837870ca693ed33ba3bf68b2a1ce4612aa075e
Sebastian Zieliński <zielis277@wp.pl>
2025-05-11 02:22:54 +0200

    Login Page

--- frontend/trAIning-buddy/src/components/userNotLoggedIn/mainPage/lookingForCoach/LookingForCoach.tsx
+++ frontend/trAIning-buddy/src/components/userNotLoggedIn/mainPage/lookingForCoach/LookingForCoach.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import "./LookingForCoach.css";
 import { Link } from "react-router-dom";
 import personalTrainer from "/personal trainer.png";
 
 function LookingForCoach() {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollPercent = Math.min(scrollY / 300, 1);
+
+  const blackStop = scrollPercent * 100;
+
+  const dynamicBackground = `linear-gradient(to right, green 0%, black ${blackStop}%)`;
+
   return (
-    <div className="coach-frame">
+    <div className="coach-frame" style={{ background: dynamicBackground }}>
       <div className="coach-text-frame">
         <div style={{ fontSize: "6em" }}>Szukasz trenera personalnego?</div>
         <div style={{ fontSize: "3em", marginTop: "10%" }}>
