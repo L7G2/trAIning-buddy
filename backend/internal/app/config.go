3b4323d03b8fecafb775a18659680b4e92300a0e
kinnaynni <kinnaynni@gmail.com>
2025-05-14 21:36:10 +0200

    added /profile endpoint

--- backend/internal/app/config.go
+++ backend/internal/app/config.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"backend/internal/domain/repositories"
 	"backend/internal/handlers"
 	"backend/internal/middleware"
 	"database/sql"
@@ -15,10 +16,19 @@ func SetupRoutes(router *gin.Engine, db *sql.DB) {
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Backend is up and running"})
 	})
+
 	auth := handlers.NewAuthHandler(db)
 	router.POST("/register", auth.Register)
 	router.POST("/login", auth.Login)
-
 	router.GET("/me", middleware.AuthMiddleware(), auth.Me)
 
+	profileRepo := repositories.NewProfileRepository(db)
+	profileHandler := handlers.NewProfileHandler(profileRepo)
+
+	authGroup := router.Group("/profile")
+	authGroup.Use(middleware.AuthMiddleware())
+	{
+		authGroup.GET("", profileHandler.GetProfile)
+		authGroup.POST("", profileHandler.SaveProfile)
+	}
 }
